a4aa79e1ee292e314afd2e40edef54382ad56c14
Garitskiy-Victor <dinomc4711@gmail.com>
2023-03-26 20:10:16 +0600

    fix bug

--- Photo Clone Collection/Sources/Constant/Constants.swift
+++ Photo Clone Collection/Sources/Constant/Constants.swift
@@ -5,4 +5,30 @@
 //  Created by Victor Garitskyu on 26.03.2023.
 //
 
-import Foundation
+import UIKit
+
+enum Constants {
+    enum Colors {
+        static var background = UIColor.white
+        static var primary = UIColor(red: 0.09, green: 0.01, blue: 0.25, alpha: 1.00)
+        static var border = UIColor.black
+        static var text = UIColor.black
+        static var lightText = UIColor.white
+        static var highlighted = UIColor.black.withAlphaComponent(0.5)
+        static var divider = UIColor.gray.withAlphaComponent(0.25)
+    }
+    
+    enum Fonts {
+        static var headerFont = UIFont.systemFont(ofSize: 36, weight: .bold)
+        static var subHeaderFont = UIFont.systemFont(ofSize: 22, weight: .bold)
+        static var title = UIFont.systemFont(ofSize: 16, weight: .regular)
+        static var subtitle = UIFont.systemFont(ofSize: 16, weight: .medium)
+        static var utilitilesTitile = UIFont.systemFont(ofSize: 20, weight: .regular)
+    }
+    
+    enum Layout {
+        static var paging: CGFloat = 36
+        static var cornerRadius: CGFloat = 8
+        static var borderWidth: CGFloat = 1.25
+    }
+}

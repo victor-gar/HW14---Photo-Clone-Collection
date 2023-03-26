fdb1b543eae6169774ea0f8d065b74a081268ba1
Garitskiy-Victor <dinomc4711@gmail.com>
2023-03-26 18:25:21 +0600

    add library View Controller

---
+++ Photo Clone Collection/View/AlbumsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AlbumsViewController.swift
+//  Photo Clone Collection
+//
+//  Created by Victor Garitskyu on 30.01.2023.
+//
+
+import UIKit
+
+class AlbumsViewController: UIViewController {
+    
+    
+    
+    
+    // MARK: - UI
+   
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+        
+    // MARK: - Setups
+    
+    private func setupView() {
+        title = "Albums"
+    }
+    
+    private func setupHierarchy() {
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+  
+    
+    
+}
+
+
